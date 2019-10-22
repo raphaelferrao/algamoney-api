8bd6dd844086a1aeb0197b919b1a0418cdb37af5
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2019-10-22 12:34:14 -0300

    implementando logout

---
+++ src/main/java/com/example/algamoney/api/controller/TokenController.java
@@ -0,0 +1,30 @@
+package com.example.algamoney.api.controller;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenController {
+
+	@DeleteMapping("/revoke")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void revoke(HttpServletRequest request, HttpServletResponse response) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(false);
+		cookie.setPath(request.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		response.addCookie(cookie);
+		response.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+	
+}
