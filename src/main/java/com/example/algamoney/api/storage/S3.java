cff65456f07fee67cb7f8ca8263e261737891dd7
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2020-07-14 19:19:49 -0300

    acerto na montagem da url para downloado de arquivos do Amazon S3

--- src/main/java/com/example/algamoney/api/storage/S3.java
+++ src/main/java/com/example/algamoney/api/storage/S3.java
@@ -83,7 +83,7 @@ public class S3 {
 	}
 	
 	public String configurarUrl(String objeto) {
-		return "\\" + apiProperty.getS3().getBucket() + ".s3.amazonaws.com/" + objeto;
+		return "https://" + apiProperty.getS3().getBucket() + ".s3.amazonaws.com/" + objeto;
 	}
 
 	private String gerarNomeUnico(String originalFilename) {
